28f06dc2d4101c9f098ebf7fe38cda8d5eac5399
Jamal Khan <jamal.h.khan@gmail.com>
2012-10-17 22:59:37 -0400

    Updating Keys

--- DeveloperMachineSetup/SetupHG.ps1
+++ DeveloperMachineSetup/SetupHG.ps1
@@ -12,7 +12,7 @@ $file = $Env:UserProfile + "\mercurial.ini"
 "username = " + $firstName + " " + $lastName + " <" + $emailAddress + ">" | Out-File $file -append -encoding ascii 
 "" | Out-File $file -append -encoding ascii 
 "[hostfingerprints]" | Out-File $file -append -encoding ascii 
-"source.bleak.homeip.net = d7:e9:f6:b5:2f:ea:01:37:f3:59:81:d9:e6:20:b4:fd:96:fc:8f:17" | Out-File $file -append -encoding ascii 
+"test.com = 00:ff:00:ff:00:ff:00:ff:00:ff:00:ff:00:ff:00:ff:00:ff:00:ff" | Out-File $file -append -encoding ascii 
 "" | Out-File $file -append -encoding ascii 
 "[extensions]" | Out-File $file -append -encoding ascii 
 "hgext.purge=" | Out-File $file -append -encoding ascii 
